b83000e8c0dd4b3dbba3eb6574a38b2986d42ec0
WladimirDuManoir <wladduma@gmail.com>
2015-03-08 16:17:36 +0100

    timer test

---
+++ Assets/4 - MecanoGame/Scripts/TimeBarscript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeBarscript : MonoBehaviour {
+
+	public RectTransform timeTransform;
+	private float cachedY;
+	private float minXValue;
+	private float maxXValue;
+	public float maxTime;
+	private float currentTime;
+
+	private int CurrentTime {
+		get { return (int)currentTime;}
+		set { currentTime = value;
+			HandleTime();
+		}
+	}
+	public Image visualtimebar;
+
+	// Use this for initialization
+	void Start () {
+		cachedY = timeTransform.position.y;
+		maxXValue = timeTransform.position.x;
+		minXValue = timeTransform.position.x - timeTransform.rect.width;
+		currentTime = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CurrentTime -= 1;
+	}
+
+	private void HandleTime () {
+		float currentXValue = MapValues (currentTime, 0, maxTime, minXValue, maxXValue);
+		timeTransform.position = new Vector3 (currentXValue, cachedY);
+		if (currentTime > maxTime/2) { // from green to yellow
+			visualtimebar.color = new Color32((byte)MapValues(currentTime,maxTime/2,maxTime,255,0),255,0,255);		
+		} else { // from yellow to red
+			visualtimebar.color = new Color32(255,(byte)MapValues(currentTime, 0, maxTime/2,0,255),0,255);
+		}
+	}
+
+
+	private float MapValues (float x, float inMin, float inMax, float outMin, float outMax) {
+		return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
+
+}
