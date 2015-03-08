c65960569ba46b9ee50778ce440741904b5cfbb5
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-08 16:49:07 +0100

    ADD scroll qui s'arrête

--- Assets/2 - PainterGame/Scripts/MoveBackground.cs
+++ Assets/2 - PainterGame/Scripts/MoveBackground.cs
@@ -5,15 +5,28 @@ public class MoveBackground : MonoBehaviour {
 	
 	// Ici on gère le déplacement du BackGround
 
-	float speed;
-
+	public float speed;
+	public float movement;
+	public float previousX;
+	public float maxMovement;
 	// Use this for initialization
 	void Start () {
 		speed = 1;
+		movement = 0;
+		previousX = this.transform.position.x;
+		// TODO modify calculation of maxMovement
+		maxMovement = this.renderer.bounds.size.x - (float)10.5 ;
 	}
 
 	void FixedUpdate() {
+
+		// We check we didn't get out off the screen
+		// Check if not out of the screen
+		if (movement < maxMovement) {
+		previousX = this.transform.position.x;
 		this.transform.Translate (Vector3.up * speed * Time.deltaTime);
+		movement += (this.transform.position.x - previousX);
+		}
 	}
 	
 	// Update is called once per frame
