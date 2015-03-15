2dd3546d57585df22afef185edbd5cb13c1b3e90
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-16 00:25:27 +0100

    ADD menu fin jeu

---
+++ Assets/2 - PainterGame/Scripts/MenuEndScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEndScript : MonoBehaviour {
+	
+	public void OnClickReplay() {
+		Application.LoadLevel ("PainterGameLvl0");
+	}
+	public void OnClickMenuPrincipal() {
+		print ("Menu principal à faire");
+	}
+}
