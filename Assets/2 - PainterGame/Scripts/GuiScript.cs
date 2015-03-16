0d33d9c250b0ba3729703ca07645cebbaa644ae8
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-16 19:28:30 +0100

    FIX taille

---
+++ Assets/2 - PainterGame/Scripts/GuiScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiScript : MonoBehaviour {
+
+	public GUISkin customSkin;
+	
+	// Apply the Skin in our OnGUI() function
+	public void OnGUI () {
+				GUI.skin = customSkin;
+		}
+}
