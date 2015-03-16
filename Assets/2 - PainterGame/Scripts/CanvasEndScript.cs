cccf037aed55f0afe3d2b457b53eb6233894c562
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-16 01:22:39 +0100

    ADD affichage score fin partie

---
+++ Assets/2 - PainterGame/Scripts/CanvasEndScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasEndScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickContinuer() {
+		Application.LoadLevel ("MenuEndPaint");
+	}
+}
