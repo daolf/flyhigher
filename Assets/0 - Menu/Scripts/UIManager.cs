73fda8f5b541501fa55e9c7a610e6b1e46868111
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-18 00:31:06 +0100

    UPD MenuMiniGames

--- Assets/0 - Menu/Scripts/UIManager.cs
+++ Assets/0 - Menu/Scripts/UIManager.cs
@@ -2,28 +2,28 @@
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
-	
-	// Update is called once per frame
+
+
+	public void Menu() {
+		Application.LoadLevel("MainMenu");
+	}
+
 	public void MainGame () {
 		Application.LoadLevel("MainGame");
 	}
 
-	// Use this for initialization
 	public void MiniGame () {
 		Application.LoadLevel("MenuMiniGames");
 	}
 	
-	// Update is called once per frame
 	public void Painter () {
 		Application.LoadLevel("PainterGameLvl0");
 	}
 
-	// Update is called once per frame
 	public void Propulsion () {
 		Application.LoadLevel("IngameScene");
 	}
 	
-	// Update is called once per frame
 	public void Mecano () {
 		Application.LoadLevel("main");
 	}
