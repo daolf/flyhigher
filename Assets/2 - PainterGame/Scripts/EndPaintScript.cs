34cb84fa73167c1bdabb3b89104d2c25c933a80a
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-17 19:03:50 +0100

    FIX bug score sur menu FIN

--- Assets/2 - PainterGame/Scripts/EndPaintScript.cs
+++ Assets/2 - PainterGame/Scripts/EndPaintScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class EndPaintScript : MonoBehaviour {
 
 	public GameObject endMenu;
+	public GameObject scoreText;
 	public Text score;
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,8 @@ public class EndPaintScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		score.text = GetComponent<MainPaintScript> ().score.ToString();
+		//On desactive l'affichage du score poru eviter les overlay avec le menu
+		scoreText.GetComponent<Canvas> ().enabled = false;
 		endMenu.GetComponent<Canvas>().enabled = true;
 	}
 }
