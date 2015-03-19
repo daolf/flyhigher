a146d905f549703fa74d3e8b1f00e1b1ebcf91f6
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-19 01:17:59 +0100

    Ajout intéraction doigt TODO ajuster param

--- Assets/1 - MainGame/Scripts/PlanePhysics.cs
+++ Assets/1 - MainGame/Scripts/PlanePhysics.cs
@@ -6,10 +6,13 @@ public class PlanePhysics : MonoBehaviour {
 	public int reactorForce;
 	private Vector2 origin;
 	public float rotationSpeed;
+	public Rigidbody2D rb;
+	public float initialGravity;
 
 	// Use this for initialization
 	void Start () {
 		origin = transform.position;
+		initialGravity = (float)0.4;
 	}
 
 	void OnEnable() {
@@ -19,10 +22,23 @@ public class PlanePhysics : MonoBehaviour {
 	void FixedUpdate() {
 
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
+		//Quand on appuie on alège la gravité
+		if (Input.GetMouseButton(0)) {
+			print ("TouchMainGame");
+			rb.gravityScale = (float)( 0.06 / -Mathf.Abs(rb.velocity.y));
+
+		}
+		//Si on appuie plus on la remet a la valeur initiale
+		else {
+			print ("NoTouch");
+			rb.gravityScale = initialGravity;
+		}
 	}
+	
 
 	public void decoller (float angle, float power) {
 		Quaternion theRotation = transform.localRotation;
@@ -30,7 +46,7 @@ public class PlanePhysics : MonoBehaviour {
 		transform.localRotation = theRotation;	
 
 		Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
-		GetComponent<Rigidbody2D>().AddForce(dir*power);
+		GetComponent<Rigidbody2D>().AddForce(dir*power*(float)0.4);
 
 	}
 
@@ -41,5 +57,7 @@ public class PlanePhysics : MonoBehaviour {
 	public float getDistanceFromOrigin() {
 		return Vector2.Distance(transform.position, this.origin);
 	}
+
+
 
 }
---
+++ Assets/PlayerPref/ConstSave.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public static class Constants {
+	
+
+	//Const Main game
+	public const string MAIN_GAME_HIGH_SCORE = "MAIN_GAME_HIGH_SCORE"; 	
+	public const string	MAIN_GAME_BONUS_LIFE = "MAIN_GAME_BONUS_LIFE";
+	
+}
