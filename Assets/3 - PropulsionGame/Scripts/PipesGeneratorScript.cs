5b893ccce208428c705623cbf730cf516b7ecfa2
Leo Grange <grangeleo@gmail.com>
2015-03-31 00:41:14 +0200

    add load/save of maximum difficulty allowed for Propulsion game

--- Assets/3 - PropulsionGame/Scripts/PipesGeneratorScript.cs
+++ Assets/3 - PropulsionGame/Scripts/PipesGeneratorScript.cs
@@ -154,6 +154,14 @@ public class PipesGeneratorScript : MonoBehaviour {
 	
 	// internal : called when "you win" message is ready to be displayed
 	private void onEffectiveWin() {
+		// save the maximum level allowed if needed
+		int currentMaxLevel = PlayerPrefs.GetInt(Constants.PROPULSION_GAME_MAX_DIFFICULTY);
+		if(currentMaxLevel < (currentDifficulty + 1) && currentMaxLevel < 3) {
+			PlayerPrefs.SetInt(Constants.PROPULSION_GAME_MAX_DIFFICULTY, currentDifficulty + 1);
+			// FIXME not a good place to be called?
+			PlayerPrefs.Save();
+		}
+	
 		GameObject.Find("WinMenu").GetComponent<Canvas>().enabled = true;
 	}
 	
@@ -224,6 +232,9 @@ public class PipesGeneratorScript : MonoBehaviour {
 	void Start () {
 		// On zoom
 		myCamera.to = 3.22f;
+		
+		// TODO ser may select a lower difficulty level?
+		currentDifficulty = PlayerPrefs.GetInt(Constants.PROPULSION_GAME_MAX_DIFFICULTY, 1);
 
 		parentArea = GameObject.Find("/Container").transform;
 		grid = instanciateLevelFromXml (getRandomLevel(currentDifficulty));
--- Assets/PlayerPref/ConstSave.cs
+++ Assets/PlayerPref/ConstSave.cs
@@ -8,5 +8,8 @@ public static class Constants {
 	//Const Main game
 	public const string MAIN_GAME_HIGH_SCORE = "MAIN_GAME_HIGH_SCORE"; 	
 	public const string	MAIN_GAME_MAX_LIFE = "MAIN_GAME_MAX_LIFE";
+	
+	// Propulsion game
+	public const string PROPULSION_GAME_MAX_DIFFICULTY = "PROPULSION_GAME_MAX_DIFFICULTY";	
 
 }
