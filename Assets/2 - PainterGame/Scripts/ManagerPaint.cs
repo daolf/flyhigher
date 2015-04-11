5c9056d7d130e912107ea6af8f13673886b69207
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-11 17:05:05 +0200

    paint game ne freeze plus au demarrage

--- Assets/2 - PainterGame/Scripts/ManagerPaint.cs
+++ Assets/2 - PainterGame/Scripts/ManagerPaint.cs
@@ -19,6 +19,7 @@ public class ManagerPaint : MonoBehaviour {
 		mainPaintScript.enabled = false;
 		endLooseMenu.enabled = false;
 		endWinMenu.enabled = false;
+		Time.timeScale = 1;
 	}
 	
 	// Update is called once per frame
