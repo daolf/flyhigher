88e55e518cbfa92f4c965e2be268206534df4a4a
Leo Grange <grangeleo@gmail.com>
2015-04-11 23:16:04 +0200

    add some scripts for tuto

--- Assets/UiFinale/Scripts/AutoType.cs
+++ Assets/UiFinale/Scripts/AutoType.cs
@@ -5,12 +5,30 @@ using UnityEngine.UI;
 public class AutoType : MonoBehaviour {
 	
 	public float letterPause;
+	public bool autoStart = true;
 	
-	string message;
+	private string message;
+	
 
 	// Use this for initialization
 	void Start () {
 		message = GetComponent<Text>().text;
+		if(autoStart)
+			startTyping();
+	}
+	
+	// set a new message to display
+	public void setMessage(string newMessage) {
+		message = newMessage;
+	}
+	
+	// reset the content of Text area to be able to type it again
+	public void resetText() {
+		// TODO find a way to stop coroutine???
+		GetComponent<Text>().text = "";
+	}
+	
+	public void startTyping() {
 		GetComponent<Text>().text = "";
 		StartCoroutine(TypeText ());
 	}
---
+++ Assets/UiFinale/Scripts/GenericTutoScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericTutoScript : MonoBehaviour {
+
+	public AutoType textField;
+
+	// Use this for initialization
+	void Start () {
+		LeanTween.move(gameObject.GetComponent<RectTransform>(), new Vector2(258, 152), 1.5f)
+			.setEase(LeanTweenType.easeInOutQuint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
