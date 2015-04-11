82936d2c1d45c2bc4f910b05f25a9641a99002dc
Leo Grange <grangeleo@gmail.com>
2015-04-11 16:00:09 +0200

    add a sound in propulsion game played when win path is displayed
    This sound is generated with a compressed sinusoid, and have no harmonics,
    so I'm not sure at all if it may be pleasant (with some adjustement?)
    For now, frequency used are from 220Hz to 440Hz (from A3 to A4), and may
    be changed in editor if needed.
    Need an additionnal check and maybe should be remove?

---
+++ Assets/3 - PropulsionGame/Scripts/SineVariableAudioSource.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineVariableAudioSource : MonoBehaviour {
+	public float frequencyIn = 220;
+	public float frequencyOut = 440;
+	
+	public float duration = 5;
+	
+	private int position = 0;
+	private int sampleRate = 44100;
+	private AudioClip internalAudioClip;
+
+	// Use this for initialization
+	void Start () {
+		setDuration(duration);
+		//source.Play();
+	}
+	
+	public void setDuration(float newDuration) {
+		duration = newDuration;
+		// very, verrry ugly, but no other simple way! Thanks Unity.
+		internalAudioClip = AudioClip.Create("VariableSinusoid", (int)(sampleRate*duration), 1,
+		                                     sampleRate, true, audioReadCallback, audioSetPositionCallback);
+		GetComponent<AudioSource>().clip = internalAudioClip;
+	}
+
+	void audioReadCallback(float[] data) {
+		int count = 0;
+		float lnfact = (float)duration / Mathf.Log(frequencyOut/frequencyIn);
+		while (count < data.Length) {
+			float currentTime = (float)position / (float)sampleRate;
+			data[count] = Mathf.Sin(2 * Mathf.PI * frequencyIn * currentTime * Mathf.Exp(currentTime / lnfact));
+			position++;
+			count++;
+		}
+	}
+	
+	void audioSetPositionCallback(int position) {
+		this.position = position;
+	}
+}
