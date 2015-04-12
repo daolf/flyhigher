fe06d31e8d0de478962c362523dceac3b87186f4
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-12 23:55:23 +0200

    UPD message informatif du peintre

--- Assets/2 - PainterGame/Scripts/ManagerPaint.cs
+++ Assets/2 - PainterGame/Scripts/ManagerPaint.cs
@@ -111,20 +111,24 @@ public class ManagerPaint : MonoBehaviour {
 		tutoScript.setBubbleVisibility(false);
 		tutoScript.readyCallback = delegate() {
 			tutoScript.setBubbleVisibility(true);
+			string[] messages;
 			//si c'est la première fois présentation , 
 			if(PlayerPrefs.GetInt (Constants.PAINT_GAME_ALREADY_PLAYED) == 0 ) {
-				tutoScript.say("Bonjour, je m'appelle Victor et je suis peintre aéronautique.. Mon métier consiste à peindre toutes les parties de l'avion. Attention, ce métier n'est pas aussi simple qu'il n'y parait.");
+				messages = new string[]{"Bonjour, je m'appelle Victor et je suis peintre aéronautique. Mon métier consiste à peindre toutes les parties de l'avion., Attention, ce métier n'est pas aussi simple qu'il n'y parait.",
+					"En effet, je dois effectuer les opérations de préparation, de traitement et de finition de la surface de l'avion. J'effectue aussi les finitions de l'aéronef."};
 			}
 			else {
 				float r = Random.value;
 				print (r);
 				if (r < 0.5) {
-					tutoScript.say("Cas 1");
+					messages = new string[]{"Pour travailler dans ce domaine, il faut avoir le goût du travail manuel et être extremement précis !",
+						"Il faut savoir lire les plans d'un avion et respecter scrupuleusement les consignes de sécurité."};
 				}
 				else {
-					tutoScript.say("Cas 2");
+					messages = new string[]{"Pour être peintre aéronautique, il est nécessaire d'avoir une bonne connaissance des techniques de peinturage ainsi que des produits manipulés."};
 				}
 			}
+			tutoScript.say(messages);
 			PlayerPrefs.SetInt(Constants.PAINT_GAME_ALREADY_PLAYED,1);
 			
 		};
