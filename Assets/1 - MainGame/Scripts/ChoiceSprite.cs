5632f2637509ae71640cd6ab0f5dd601a7368095
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-12 12:11:18 +0200

    gestion bonus

---
+++ Assets/1 - MainGame/Scripts/ChoiceSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoiceSprite : MonoBehaviour {
+
+	public Sprite lvl0;
+	public Sprite lvl1;
+	public Sprite lvl2;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.GetInt(Constants.PAINT_GAME_LVL3_UNLOCK) == 1 ) {
+			GetComponent<SpriteRenderer>().sprite = lvl2;
+		}
+		else if (PlayerPrefs.GetInt(Constants.PAINT_GAME_LVL2_UNLOCK) == 1 ) {
+			GetComponent<SpriteRenderer>().sprite = lvl1;
+		}
+		else  {
+			GetComponent<SpriteRenderer>().sprite = lvl0;
+		}
+		Destroy(GetComponent<PolygonCollider2D>());
+		gameObject.AddComponent<PolygonCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
