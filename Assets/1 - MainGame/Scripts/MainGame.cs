db3a23c90aa56d257d11c30187c121cb5395a64c
Jules Libert <jules.lib@gmail.com>
2015-04-13 16:47:06 +0200

    FIX BUG Plane liftoff

--- Assets/1 - MainGame/Scripts/MainGame.cs
+++ Assets/1 - MainGame/Scripts/MainGame.cs
@@ -151,10 +151,11 @@ public class MainGame : MonoBehaviour {
 				score = scriptPlanePhysics.getDistanceFromOrigin ();
 				setScore (score);
 
-			//Si on s'arrête sur la piste d'aterrisage
-				if (scriptPlanePhysics.rb.velocity.x <= 0.2) {
+				//Si on s'arrête sur la piste d'aterrisage
+				if (scriptPlanePhysics.rb.velocity.x <= 0.3) {
 					state = State.END_LOOSE;
 				}
+
 			//On passe a l'état main quand on arrete de monter
 
 				if (scriptPlanePhysics.transform.position.y < prevY) {
@@ -172,7 +173,10 @@ public class MainGame : MonoBehaviour {
 					criticalPanel.criticalState = CriticalPanelScript.CriticalState.NORMAL;
 				}
 			
-			
+				//Si on s'arrête sur la piste d'aterrisage
+				if (scriptPlanePhysics.rb.velocity.x <= 0.3) {
+					state = State.END_LOOSE;
+				}
 
 			//Wait for the fuel not to be consume with the first on touch
 				scriptFuelControl.enabled = true;
